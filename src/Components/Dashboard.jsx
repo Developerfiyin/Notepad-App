b6b86cadade409e5be5d44236397c73c33c0bc5f
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-13 19:43:21 +0100

    Added a section to the dashboard.jsx file

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import image1 from "../assets/image1.png";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <section className="bg-black py-10 px-6 ">
+      <div className="flex gap-5 border-b border-white/55">
+        <GiHamburgerMenu size={30} color="white" />
+        <img src={image1} alt="THE NOTE IMAGES" />
 
-export default Dashboard
+      </div>
+    </section>
+  );
+};
+
+export default Dashboard;
