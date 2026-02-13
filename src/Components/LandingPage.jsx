15ef86485f543553721e0e3386a9ca7ada18f490
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-13 15:03:09 +0100

    Added a child div into the parent div containing some details

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 const LandingPage = () => {
   return (
-    <main className='bg-black w-full py-7 px-6'>
-         <div className='mx-50 text-white bg-red-500 my-auto  text-center'> 
-            <h5>Let's Start</h5>
+    <main className='bg-black w-full h-screen items-center py-16 px-6'>
+         <div className='mx-100 gap-4 font-semibold text-white rounded-xl border border-gray-500 py-12 flex flex-col bg-black text-center'> 
+            <h5 className='text-base text-green-500 '>Let's Start</h5>
+            <h2 className='text-2xl '>How do you plan to use Notetaker?</h2>
+            <h4 className='text-base text-white/50'>We'll redirect you to the best setup for your needs.</h4>
+
+            <button className='items-center flex gap-5 mx-auto'>
+              <div className='h-5 w-5 rounded-full bg-green-500' ></div>  
+                <h3>For my productivity</h3>
+                
+           </button>
          </div>
     </main>
   )
