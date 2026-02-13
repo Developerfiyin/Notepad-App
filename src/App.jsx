b7ca035a44910df17588805208fffd0705cbd8af
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-13 14:39:53 +0100

    Styled global css

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
         <Routes>
-         <Route path="/landing" element={<LandingPage/>} />
+         <Route path="/" element={<LandingPage/>} />
 
         </Routes>
     </>
--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <main className='bg-black w-full py-7 px-6'>
+         <div className='mx-50 text-white bg-red-500 my-auto  text-center'> 
+            <h5>Let's Start</h5>
+         </div>
+    </main>
   )
 }
 
