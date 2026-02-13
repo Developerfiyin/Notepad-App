9b35a89630e7bd8386c93b1c319b2d70b7a8b3f9
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-13 14:30:35 +0100

    Setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
+import { Routes, Route } from 'react-router-dom';
 import LandingPage from "./Components/LandingPage";
 import "./App.css";
+
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-         <LandingPage/>
-      </div>
+        <Routes>
+         <Route path="/landing" element={<LandingPage/>} />
+
+        </Routes>
     </>
   );
 }
