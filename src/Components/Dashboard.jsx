370e72443b572fa1ff6b1aaf12b656919d890172
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-13 19:57:51 +0100

    Updated  section in dashboard.jsx file

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -4,11 +4,16 @@ import image1 from "../assets/image1.png";
 
 const Dashboard = () => {
   return (
-    <section className="bg-black py-10 px-6 ">
+    <section className="bg-black py-10 px-6 text-white">
       <div className="flex gap-5 border-b border-white/55">
         <GiHamburgerMenu size={30} color="white" />
-        <img src={image1} alt="THE NOTE IMAGES" />
-
+        <img src={image1} alt="THE NOTE IMAGES" className="mb-9" />
+        <h2 className="font-medium text-2xl">Notetaker App</h2>
+        <input
+          name="title"
+          placeholder="eg, Favour can be a goat sometimes.."
+          className='w-100 h-11 pl-10 focus:outline-green-400 border-white/60 pr-3 border rounded-md text-sm leading-none"'
+        />
       </div>
     </section>
   );
