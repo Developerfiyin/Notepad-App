62755911527505e96eb071501d9fbf30bdd5a154
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-13 23:19:13 +0100

    Updated div in dashboard.jsx

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { LuSearch } from "react-icons/lu";
+import { BsViewStacked } from "react-icons/bs";
+
 
 import image1 from "../assets/image1.png";
 
@@ -15,41 +17,22 @@ const Dashboard = () => {
         <label className="relative block">
           <LuSearch
             size={18}                                                       
-            className="absolute left-3 top-1/2 -translate-y-1/2 text-green-400 pointer-events-none"
+            className="absolute left-3 top-1/4 -translate-y-1/2 text-green-400 pointer-events-none"
           />
           <input
             type="text"
             placeholder="Search..."
-            className="w-120 h-11 pl-10 focus:outline-green-400 border-white/55 pr-3 border rounded-md text-sm leading-none"
-            
-
+            className="w-120 h-11 pl-19 focus:outline-green-400 border-white/55 pr-3 border rounded-lg text-sm leading-none"
           />
         </label>
 
+         
+       
 
 
 
 
-
-    <div className="relative w-full max-w-md">
-      {/* 1. The Icon Container */}
-      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-        <LuSearch className="w-5 h-5 text-gray-400" />
-      </div>
-
-      {/* 2. The Input Tag */}
-      <input
-        type="search"
-        className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
-        placeholder="Search notes..."
-        required
-      />
-    </div>
-  
-
-
-
-
+        
       </div>
     </section>
   );
