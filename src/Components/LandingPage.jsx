c9fa6cfc60e2b4c37b0b6d5b30c72e9ed37dd433
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-13 15:37:55 +0100

    updated the child div into the parent div containing some details

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -11,10 +11,24 @@ const LandingPage = () => {
           We'll redirect you to the best setup for your needs.
         </h4>
 
-        <button className="items-center justify-around hover:border-green-500 flex gap-5 border border-white/45 py-6 px-10 rounded-2xl mx-auto">
+        <button className="items-center justify-around hover:border-green-500 flex gap-5 border border-white/45 py-6 px-14 rounded-2xl mx-auto">
           <div className="h-4 w-4 rounded-full bg-green-500"></div>
           <h3>For my productivity</h3>
           <FaArrowRightLong />
+        </button>
+
+        <button className="items-center justify-around group transition-all duration-300 hover:border-green-500 flex gap-5  border border-white/45 py-6 px-24 rounded-2xl mx-auto">
+          <div className=" space-y-1 
+          
+          ">
+            <div className="h-3 w-3 ml-2 rounded-full bg-orange-500"></div>
+            <div className="h-3 w-3  rounded-full bg-orange-500"></div>
+          </div>
+          <h3>For work</h3>
+          <FaArrowRightLong
+           
+          
+          className="transition-transform duration-300 transform group-hover:translate-x-1.5" />
         </button>
       </div>
     </main>
