161d3fc18897f552fa3066cee8ffe0de8df8f8d1
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-13 15:52:19 +0100

    updated the child div into the parent div containing some details

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { FaArrowRightLong } from "react-icons/fa6";
 
 const LandingPage = () => {
+  const { user } = useAuth(); // Your custom auth hook
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate("/dashboard");
+    }
+  }, [user]);
   return (
     <main className="bg-black/90 w-full h-screen items-center py-16 px-6">
       <div className="mx-100 gap-4 font-semibold text-white rounded-2xl border border-gray-500 py-12 flex flex-col bg-black/60 text-center">
@@ -11,25 +21,25 @@ const LandingPage = () => {
           We'll redirect you to the best setup for your needs.
         </h4>
 
-        <button className="items-center justify-around group transition-all duration-300 hover:border-green-500 flex gap-5 border border-white/45 py-6 px-14 rounded-2xl mx-auto">
+        <button
+          onClick={() => navigate("/signup")}
+          className="items-center justify-around group transition-all duration-300 hover:border-green-500 flex gap-5 border border-white/45 py-6 px-14 rounded-2xl mx-auto"
+        >
           <div className="h-4 w-4 rounded-full bg-green-500"></div>
           <h3>For my productivity</h3>
-          <FaArrowRightLong  
-          className="transition-transform duration-300 transform group-hover:translate-x-1.5"
-           />
+          <FaArrowRightLong className="transition-transform duration-300 transform group-hover:translate-x-1.5" />
         </button>
 
         <button className="items-center justify-around group transition-all duration-300 hover:border-green-500 flex gap-5  border border-white/45 py-6 px-24 rounded-2xl mx-auto">
-          <div className=" space-y-1 
-          ">
+          <div
+            className=" space-y-1 
+          "
+          >
             <div className="h-3 w-3 ml-2 rounded-full bg-orange-500"></div>
             <div className="h-3 w-3  rounded-full bg-orange-500"></div>
           </div>
           <h3>For work</h3>
-          <FaArrowRightLong
-           
-          
-          className="transition-transform duration-300 transform group-hover:translate-x-1.5" />
+          <FaArrowRightLong className="transition-transform duration-300 transform group-hover:translate-x-1.5" />
         </button>
       </div>
     </main>
