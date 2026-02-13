31088395ed3c2ce596af7413846073704d134444
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-13 20:31:47 +0100

    Updated  section in dashboard.jsx file

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { LuSearch } from "react-icons/lu";
+
 import image1 from "../assets/image1.png";
 
 const Dashboard = () => {
@@ -8,12 +10,21 @@ const Dashboard = () => {
       <div className="flex gap-5 border-b border-white/55">
         <GiHamburgerMenu size={30} color="white" />
         <img src={image1} alt="THE NOTE IMAGES" className="mb-9" />
-        <h2 className="font-medium text-2xl">Notetaker App</h2>
-        <input
-          name="title"
-          placeholder="eg, Favour can be a goat sometimes.."
-          className='w-100 h-11 pl-10 focus:outline-green-400 border-white/60 pr-3 border rounded-md text-sm leading-none"'
-        />
+        <h2 className="font- medium text-2xl">Notetaker App</h2>
+
+        <label className="relative block">
+          <LuSearch
+            size={18}                                                       
+            className="absolute left-3 top-1/2 -translate-y-1/2 text-green-400 pointer-events-none"
+          />
+          <input
+            type="text"
+            placeholder="Search..."
+            className="w-120 h-11 pl-10 focus:outline-green-400 border-white/55 pr-3 border rounded-md text-sm leading-none"
+            
+
+          />
+        </label>
       </div>
     </section>
   );
