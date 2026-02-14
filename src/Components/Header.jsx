b8cd55a834fd89993bd9622c9f33bd11f2a41285
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-14 17:59:11 +0100

    Updated  header.jsx file

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,7 @@
 import { GiHamburgerMenu } from "react-icons/gi";
 import { LuSearch } from "react-icons/lu";
 import { IoSettings } from "react-icons/io5";
-
 import { MdOutlineViewAgenda } from "react-icons/md";
-
 import { MdGridView } from "react-icons/md";
 import { useState } from "react";
 
@@ -13,16 +11,16 @@ const Header = () => {
   const [isGrid, setIsGrid] = useState(false);
 
   return (
-    <section className="bg-black py-6 px-6 text-white">
-      <div className="flex justify-evenly gap-5 border-b border-white/55">
+    <section className="bg-black border-white/55 border-b  text-white sticky top-0 z-50">
+      <div className="flex mx-auto px-4 h-20 items-center max-w-7xl justify-evenly gap-5 ">
         <GiHamburgerMenu size={30} color="white" />
-        <img src={image1} alt="THE NOTE IMAGES" className="mb-9" />
+        <img src={image1} alt="THE NOTE IMAGES" className="" />
         <h2 className="font- medium text-2xl">Notetaker App</h2>
 
         <label className="relative block">
           <LuSearch
             size={18}
-            className="absolute left-3 top-1/4 -translate-y-1/2 text-green-400 pointer-events-none"
+            className="absolute left-3 top-1/2 -translate-y-1/2 text-green-400 pointer-events-none"
           />
           <input
             type="text"
@@ -33,7 +31,7 @@ const Header = () => {
 
         <button
           onClick={() => setIsGrid(!isGrid)}
-          className="mb-9 ml-8 rounded-lg hover:bg-black/65  transition-colors  outline-none"
+          className=" ml-8 rounded-lg hover:bg-black/65  transition-colors  outline-none"
           aria-label="Toggle View"
         >
           {isGrid ? (
