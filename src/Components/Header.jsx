0f7fa28f831f175c27a4474dfddc897a27119fa0
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-14 23:58:51 +0100

    Styled the div containing the buttons and icons

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -63,8 +63,8 @@ const Header = () => {
         />
       </div>
 
-      <div className=" bg-black/55 space-y-2 h-100 flex-col w-90  ">
-        <div className="flex p-2  rounded-full gap-5 bg-pink-500">
+      <div className=" bg-black/55 space-y-0.5  h-100 flex-col w-90  ">
+        <div className="flex p-2  rounded-full gap-5 bg-yellow-950 ">
           <FaLightbulb
             size={24}
             className=" mx-10 h-10 w-10 p-1 text-white/80 rounded-full bg-orange-400 "
@@ -72,28 +72,28 @@ const Header = () => {
           <button className="font-semibold text-xl">Notes</button>
         </div>
 
-        <div className=" bg-red-400 p-2  rounded-full gap-5 flex ">
+        <div className=" hover:bg-white/20 p-2  rounded-full gap-5 flex ">
           <FaBell size={24} className="mx-10 h-10 w-10 p-1 text-white/80 " />
           <button className="font-semibold text-xl">Notification</button>
         </div>
 
-        <div className="flex gap-5 p-2  rounded-full bg-blue-600">
-          <MdEdit size={30} className="mx-10 h-10 w-10 p-1 text-white/80  " />
+        <div className="flex gap-5 p-2  rounded-full hover:bg-white/20">
+          <MdEdit size={24} className="mx-10 h-10 w-10 p-1 text-white/80  " />
           <button className="font-semibold  text-xl"> Edit Label </button>
         </div>
 
-        <div className="flex gap-5 p-2  rounded-full bg-green-300">
+        <div className="flex gap-5 p-2  rounded-full hover:bg-white/20">
           <button type="button">
             <IoMdArchive
-              size={30}
+              size={24}
               className="mx-10 h-10 w-10 p-1 text-white/80"
             />
           </button>
           <button className="font-semibold text-xl"> Archive </button>
         </div>
 
-        <div className="flex gap-5 p-2 rounded-full bg-amber-400">
-          <IoTrash size={30} className="mx-10 h-10 w-10 p-1 text-white/80 " />
+        <div className="flex gap-5 p-2 rounded-full hover:bg-white/20">
+          <IoTrash size={24} className="mx-10 h-10 w-10 p-1 text-white/80 " />
           <button className="font-semibold text-xl"> Trash </button>
         </div>
       </div>
