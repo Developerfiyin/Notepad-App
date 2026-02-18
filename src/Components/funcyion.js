ebd7a0b273cb5a3107e566f8208c194711380452
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-18 13:29:00 +0100

    Added a function.js file

---
+++ src/Components/funcyion.js
@@ -0,0 +1,11 @@
+// 1. We declare the function and name its placeholders (a and b)
+function multiply(num1, num2) {
+  // 2. We perform the math and "send back" the result
+  return num1 * num2;
+}
+
+// 3. we call the function with real numbers (arguments)
+let result = multiply(5, 6);
+
+// 4. Let's see if it worked
+console.log(result); // This will show 30 in the console
