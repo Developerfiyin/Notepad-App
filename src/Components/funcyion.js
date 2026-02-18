16beb33b1d011ea1b92013044440d34d0b7836d9
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-18 22:40:00 +0100

    Updated  function.js file

--- src/Components/funcyion.js
+++ src/Components/funcyion.js
@@ -1,6 +1,21 @@
-export default function condition() {
+export default function calculateTotal(price) {
 
+    
+    if (price >= 100) {
+        let dicountedPrice = price * 0.8;
+        return dicountedPrice 
+    } else {
+       return price 
+    }
     
 }
 
-alert(age);
+;
+function calculateTotasl(price) {
+  if (price >= 100) {
+    let discountPrice = price * 0.8;
+    return "Discount applied! Your total is: $" + discountPrice;
+  } 
+  return "No discount. Total: $" + price;
+}
+alert (calculateTotasl(200));
