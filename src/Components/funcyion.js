5e30586674bce25acc64c296499cbd3affad900b
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-18 22:03:21 +0100

    Updated  function.js file

--- src/Components/funcyion.js
+++ src/Components/funcyion.js
@@ -1,10 +1,5 @@
 export default function condition() {
-let age = 31
-    if (age > 18) {
-        return "You are very much welcome"
-    } else {
-        return " Don't come close to the gate.!"
-    }
+
     
 }
 
